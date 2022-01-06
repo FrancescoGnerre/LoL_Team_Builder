9bb8607df13aa888161c5767405d864b9667d411
Dennis(Zhuo) Liu <93283680+MindUnshackled@users.noreply.github.com>
2022-01-05 17:47:03 -0800

    Update funcs.py
    Added the long elif forest for specification of elo

--- funcs.py
+++ funcs.py
@@ -19,35 +19,108 @@ def total_average_elo(list):
 def player_ELO(player):
     elo = 0.0
 
-    if player.Soloqueue == "Iron":
+    if player.Soloqueue == "Iron4":
         elo += 1
-    elif player.Soloqueue == "Bronze":
+    elif player.Soloqueue == "Iron3":
+        elo += 1.25
+    elif player.Soloqueue == "Iron2":
+        elo += 1.5
+    elif player.Soloqueue == "Iron1":
+        elo += 1.75
+    elif player.Soloqueue == "Bronze4":
         elo += 2
-    elif player.Soloqueue == "Silver":
+    elif player.Soloqueue == "Bronze3":
+        elo += 2.25
+    elif player.Soloqueue == "Bronze2":
+        elo += 2.5
+    elif player.Soloqueue == "Bronze1":
+        elo += 2.75
+    elif player.Soloqueue == "Silver4":
         elo += 3
-    elif player.Soloqueue == "Gold":
+    elif player.Soloqueue == "Silver3":
+        elo += 3.25
+    elif player.Soloqueue == "Silver2":
+        elo += 3.5
+    elif plauer.Soloqueue == "Silver1":
+        elo += 3.75
+    elif player.Soloqueue == "Gold4":
         elo += 4
-    elif player.Soloqueue == "Platinum":
+    elif player.Soloqueue == "Gold3":
+        elo += 4.25
+    elif player.Soloqueue == "Gold2":
+        elo += 4.5
+    elif player.Soloqueue == "Gold1":
+        elo += 4.75
+    elif player.Soloqueue == "Platinum4":
         elo += 5
-    elif player.Soloqueue == "Diamond":
+    elif player.Soloqueue == "Platinum3":
+        elo += 5.25
+    elif player.Soloqueue == "Platinum2":
+        elo += 5.5
+    elif player.Soloqueue == "Platinum1":
+        elo += 5.75
+    elif player.Soloqueue == "Diamond4":
         elo += 6
+    elif player.Soloqueue == "Diamond3":
+        elo += 6.25
+    elif player.Soloqueue == "Diamond2":
+        elo += 6.5
+    elif player.Soloqueue == "Diamond3":
+        elo += 6.75
     else:
         elo += 7
 
-    if player.Flexqueue == "Iron":
+if player.Soloqueue == "Iron4":
         elo += 1
-    elif player.Flexqueue == "Bronze":
+    elif player.Soloqueue == "Iron3":
+        elo += 1.25
+    elif player.Soloqueue == "Iron2":
+        elo += 1.5
+    elif player.Soloqueue == "Iron1":
+        elo += 1.75
+    elif player.Soloqueue == "Bronze4":
         elo += 2
-    elif player.Flexqueue == "Silver":
+    elif player.Flexqueue == "Bronze3":
+        elo += 2.25
+    elif player.Flexqueue == "Bronze2":
+        elo += 2.5
+    elif player.Flexqueue == "Bronze1":
+        elo += 2.75
+    elif player.Flexqueue == "Silver4":
         elo += 3
-    elif player.Flexqueue == "Gold":
+    elif player.Flexqueue == "Silver3":
+        elo += 3.25
+    elif player.Flexqueue == "Silver2":
+        elo += 3.5
+    elif plauer.Flexqueue == "Silver1":
+        elo += 3.75
+    elif player.Flexqueue == "Gold4":
         elo += 4
-    elif player.Flexqueue == "Platinum":
+    elif player.Flexqueue == "Gold3":
+        elo += 4.25
+    elif player.Flexqueue == "Gold2":
+        elo += 4.5
+    elif player.Flexqueue == "Gold1":
+        elo += 4.75
+    elif player.Flexqueue == "Platinum4":
         elo += 5
-    elif player.Flexqueue == "Diamond":
+    elif player.Flexqueue == "Platinum3":
+        elo += 5.25
+    elif player.Flexqueue == "Platinum2":
+        elo += 5.5
+    elif player.Flexqueue == "Platinum1":
+        elo += 5.75
+    elif player.Flexqueue == "Diamond4":
         elo += 6
+    elif player.Flexqueue == "Diamond3":
+        elo += 6.25
+    elif player.Flexqueue == "Diamond2":
+        elo += 6.5
+    elif player.Flexqueue == "Diamond3":
+        elo += 6.75
     else:
         elo += 7
+
 
     elo = elo/2
 
